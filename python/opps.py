4795d8414d9b7ac2b00b6a4baf992cd49f39feb2
monisha4evr <monisha.it1510@gmail.com>
2026-08-17 16:36:47 +0530

    Oops

--- python/opps.py
+++ python/opps.py
@@ -114,3 +114,225 @@ c.add(5,10)
 # 2. Abstraction - hiding unnecessary information - show important 
 # 3. Polymorphism - many form 
 # 4. Inheritance - single,multiple, multi-level,hierarchial
+
+# Encapsulation
+#-------------------
+# Access Specifier
+# 1. Public - Anyone can Access
+# 2. Protected - class and inherited class
+# 3. Private - within the same class
+
+# How to denote
+# protected variable : _ 
+# private variable : __ 
+        # 1. getter/Setter (get/set)
+        # 2. Name Mangling (_classname__privateVariableName)
+
+class UserProfile:
+    def __init__(self,uname,mbl,pwd):
+        self.uname=uname
+        self._mbl=mbl
+        self.__pwd=pwd
+
+    def get_password(self):
+        print("Password is:",self.__pwd)
+    
+    def set_password(self,newpwd):
+        self.__pwd=newpwd
+
+up=UserProfile("Kavi",'9809878909','kavi123')
+print(up.uname)
+print(up._mbl)
+print(up._UserProfile__pwd) # Name Mangling
+up.get_password()
+up.set_password("123Kavi")
+up.get_password()
+# print(up.__pwd) # It will Throw AttributeError
+
+#Inheritance
+# 1. Single inheritance
+# 2. Multiple Inheritance
+# 3. multilevel Inheritance 
+# 4. Hierarchical Inheritance
+
+# 1. Single inheritance
+# Base/Parent/Super class
+# Derived/child Class
+
+# one base Class -> one Derived Class
+
+class Parent:
+    def parent_display(self):
+        print("I am from Parent Class")
+
+# p=Parent()
+# p.parent_display()
+
+class Child(Parent):
+    def child_display(self):
+        print("I am Child")
+
+c=Child()
+c.child_display()
+c.parent_display()
+
+# 2. Multiple Inheritance
+# -> Multiple Parent => Single Child
+
+class Father:
+    def display(self):
+        print("Father")
+
+class Mother:
+    def m_display(self):
+        print("Mother")
+
+class Child(Father,Mother):
+    def c_display(self):
+        print("Child")
+
+c=Child()
+c.c_display()
+c.display()
+c.m_display()
+
+# 4. Hierarchical Inheritance
+# -> Single Parent => Multiple Child
+
+class Animal:
+    def make_sound(self):
+        print("Animals Makes Sounds")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog says boww boww")
+
+d=Dog()
+d.make_sound()
+d.bark()
+
+class Cat(Animal):
+    def meow(self):
+        print("cat says Meoeeeew")
+
+c= Cat()
+c.make_sound()
+c.meow()
+
+# 3. multilevel Inheritance
+# -> chain -> grandparent=> parent=>child=>grandchild
+
+class Grandparent:
+    def display(self):
+        print("Gparent")
+
+class Parent(Grandparent):
+    def p_parent(self):
+        print("parent")
+
+class Child(Parent):
+    def c_parent(self):
+        print("child")
+
+class GrandChild(Child):
+    def gc_parent(self):
+        print("Grand Child")
+
+c = GrandChild()
+c.display()
+c.p_parent()
+c.c_parent()
+c.gc_parent()
+
+# Polymorphism
+# 1. Overlaoding Same
+# 2. overriding
+
+# 1. Overlaoding Same
+# ------------------------
+class Calculation:
+    def add(self,a,b,c=0,d=None,*e):
+        print(a,b,c,d,e)
+
+c= Calculation()
+c.add(5,10)
+c.add(1,2,3)
+c.add(1,2,3,4)
+c.add(1,2,3,4,5,6,7,8)
+
+# 2. overriding (runtime  Polymorphism)
+class Animal:
+    def make_sound(self):
+        print("Animals Makes Sounds")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("cat says Meoeeeew")
+
+c= Cat()
+c.make_sound()
+
+# 2. Abstraction - hiding unnecessary information - show important 
+# normal method and Abstract method 
+
+# Decorator : Extend the Functionality without modifying the Original method
+
+from abc import ABC,abstractmethod
+
+class Vehicle(ABC):
+     @abstractmethod
+     def start_engine(self):
+        pass
+
+class Car(Vehicle):
+    def __init__(self,brand,color):
+        self.brand=brand
+        self.color=color
+
+    def display(self):
+        print(f"Brand Name: {self.brand} \nColor:{self.color}")
+
+    def start_engine(self):
+        print("Engine Started..................") 
+
+c=Car("BMW","Red")
+c.display()
+c.start_engine()
+
+
+class Parent:
+    def __init__(self):
+        print("Parent")
+
+class Father(Parent):
+    def __init__(self):
+        super().__init__()
+        print("Father")
+
+class Mother(Parent):
+    def __init__(self):
+        super().__init__()
+        print("mother")
+
+class Child(Father,Mother):
+    def __init__(self):
+        super().__init__()
+        print("Child")
+
+c=Child()
+# Method Resolution Order (MRO)
+print(Child.__mro__)
+
+#Super Class
+
+class Parent:
+    def display(self):
+        print("Parent")
+
+class Child (Parent):
+    def display(self):
+        super().display()
+        print("Child")
+
+c= Child()
+c.display()
